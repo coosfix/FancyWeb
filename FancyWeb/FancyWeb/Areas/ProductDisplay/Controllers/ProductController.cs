bb4f82635a089bf491d830e68289953ae1f0ba09
testxexam <testxexam@gmail.com>
2019-08-30 17:17:33 +0800

    2019.8.30

--- FancyWeb/FancyWeb/Areas/ProductDisplay/Controllers/ProductController.cs
+++ FancyWeb/FancyWeb/Areas/ProductDisplay/Controllers/ProductController.cs
@@ -6,13 +6,13 @@ using System.Web.Mvc;
 using FancyWeb.Models;
 using FancyWeb.Areas.ProductDisplay.Models;
 using FancyWeb.Areas.ProductDisplay.ViewModels;
-
+using System.ComponentModel;
 
 namespace FancyWeb.Areas.ProductDisplay.Controllers
 {
     public class ProductController : Controller
     {
-        public FancyStoreEntities db;
+        public FancyStoreEntities db = new FancyStoreEntities();
 
         // GET: Product
         public ActionResult Index()
@@ -22,74 +22,60 @@ namespace FancyWeb.Areas.ProductDisplay.Controllers
 
         public ActionResult Browse(int id = 1, int? sid = 0)
         {
-            using (db = new FancyStoreEntities())
-            {
-                ViewBag.categorymid = id;
-                ViewBag.categorymname = db.CategoryMiddles.Find(id).CategoryMName;
+            ViewBag.categorymid = id;
+            ViewBag.categorymname = db.CategoryMiddles.Find(id).CategoryMName;
 
-                TempData["oid"] = (Request.Cookies["oid"] == null) ? "1" : Request.Cookies["oid"].Values[0].ToString();
+            TempData["oid"] = (Request.Cookies["oid"] == null) ? "1" : Request.Cookies["oid"].Values[0].ToString();
 
-                ViewBag.categorysid = (sid != 0) ? sid : 0;
-                ViewBag.categorysname = (sid != 0) ? db.CategorySmalls.Find(sid).CategorySName : null;
+            ViewBag.categorysid = (sid != 0) ? sid : 0;
+            ViewBag.categorysname = (sid != 0) ? db.CategorySmalls.Find(sid).CategorySName : null;
 
-                return View();
-            }
+            return View();
         }
 
         public ActionResult GetActivity(int id = 0)
         {
-            using (db = new FancyStoreEntities())
-            {
-                var activities = db.Activities.Select(a => new { a.ActivityID, a.ActivityName });
-                return Json(activities.ToList(), JsonRequestBehavior.AllowGet);
-            }
+            var activities = db.Activities.Select(a => new { a.ActivityID, a.ActivityName });
+            return Json(activities.ToList(), JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult GetCategory(int id = 1)
         {
-            using (db = new FancyStoreEntities())
-            {
-                //var categories = db.CategorySmalls.Where(c => c.CategoryMID == id).GroupBy(c => c.CategorySID).Select(g => new { CategorySID = g.Key, g.FirstOrDefault().CategoryMID, g.FirstOrDefault().CategorySName, count = g.FirstOrDefault().Products.Count() });
-                var categories = db.CategorySmalls.GroupBy(c => c.CategorySID).Select(g => new { CategorySID = g.Key, g.FirstOrDefault().CategoryMID, g.FirstOrDefault().CategorySName, count = g.FirstOrDefault().Products.Count() });
-                return Json(categories.ToList(), JsonRequestBehavior.AllowGet);
-            }
+            //var categories = db.CategorySmalls.Where(c => c.CategoryMID == id).GroupBy(c => c.CategorySID).Select(g => new { CategorySID = g.Key, g.FirstOrDefault().CategoryMID, g.FirstOrDefault().CategorySName, count = g.FirstOrDefault().Products.Count() });
+            var categories = db.CategorySmalls.GroupBy(c => c.CategorySID).Select(g => new { CategorySID = g.Key, g.FirstOrDefault().CategoryMID, g.FirstOrDefault().CategorySName, count = g.FirstOrDefault().Products.Count() });
+            return Json(categories.ToList(), JsonRequestBehavior.AllowGet);
+
         }
 
         public ActionResult GetProduct(int id = 1, int sid = 0, int orderid = 1)
         {
-            using (db = new FancyStoreEntities())
-            {
-                IQueryable<ProductColor> preproducts = (sid != 0) ? db.ProductColors.Where(p => p.Product.CategorySID == sid) : db.ProductColors.Where(p => p.Product.CategorySmall.CategoryMID == id);
+            IQueryable<ProductColor> preproducts = (sid != 0) ? db.ProductColors.Where(p => p.Product.CategorySID == sid) : db.ProductColors.Where(p => p.Product.CategorySmall.CategoryMID == id);
 
-                var products = ProductMethod.CreateProductCells(preproducts);
+            var products = ProductMethod.CreateProductCells(preproducts);
 
-                var orderresult = ProductMethod.SetCellsByOrder(products, orderid);
+            var orderresult = ProductMethod.SetCellsByOrder(products, orderid);
 
-                return Json(orderresult.ToList(), JsonRequestBehavior.AllowGet);
-            }
+            return Json(orderresult.ToList(), JsonRequestBehavior.AllowGet);
+
         }
 
         public ActionResult GetSale(int id)
         {
-            using (db = new FancyStoreEntities())
-            {
-                var sale = db.ActivityProducts.Where(a => a.ProductID == id).First().Activity.DiscountMethod.Discount;
-                return Json(sale, JsonRequestBehavior.AllowGet);
-            }
+            var sale = db.ActivityProducts.Where(a => a.ProductID == id).First().Activity.DiscountMethod.Discount;
+            return Json(sale, JsonRequestBehavior.AllowGet);
+
         }
 
         public ActionResult ByteImage(int id, int? colorid = 0)
         {
-            using (db = new FancyStoreEntities())
+            if (colorid == 0)
             {
-                if (colorid == 0)
-                {
-                    colorid = db.ProductColors.Where(c => c.ProductID == id).Select(c => c.ColorID).First();
-                }
-                int? pid = db.ProductColors.Where(p => p.ProductID == id && p.ColorID == colorid).Select(p => p.PhotoID).First();
-                byte[] by = db.Photos.Find(pid).Photo1;
-                return File(by, "Image/jpeg");
+                colorid = db.ProductColors.Where(c => c.ProductID == id).Select(c => c.ColorID).First();
             }
+            int? pid = db.ProductColors.Where(p => p.ProductID == id && p.ColorID == colorid).Select(p => p.PhotoID).First();
+            byte[] by = db.Photos.Find(pid).Photo1;
+            return File(by, "Image/jpeg");
+
         }
 
         public ActionResult GetProductDetail(int id = 1, int? colorid = 0)
@@ -111,8 +97,6 @@ namespace FancyWeb.Areas.ProductDisplay.Controllers
                 recent.Add(id);
                 Session.Add("recent", recent);
             }
-
-            db = new FancyStoreEntities();
 
             var product = db.Products.Find(id);
             ProductDetail prodcutDisplay;
@@ -186,167 +170,156 @@ namespace FancyWeb.Areas.ProductDisplay.Controllers
 
             ViewBag.recommend = citems.ToList();
 
+            var evaluation = ProductMethod.LoadEvaluation(id);
+            ViewBag.evaluation = evaluation.ToList();
+
             return View(prodcutDisplay);
 
         }
 
         public ActionResult CheckQTY(int ProductID, int ColorID, int SizeID, int StockQTY)
         {
-            using (db = new FancyStoreEntities())
-            {
-                var qty = db.ProductStocks.Where(s => s.ProductID == ProductID && s.ProductColor.ColorID == ColorID && s.ProductSize.SizeID == SizeID).FirstOrDefault().StockQTY;
-                if (qty >= StockQTY)
-                    return Json("enough");
-                else
-                    return Json("preorder");
-            }
+            var qty = db.ProductStocks.Where(s => s.ProductID == ProductID && s.ProductColor.ColorID == ColorID && s.ProductSize.SizeID == SizeID).FirstOrDefault().StockQTY;
+            if (qty >= StockQTY)
+                return Json("enough");
+            else
+                return Json("preorder");
+
         }
 
         public ActionResult AddCart(CartItem item)
         {
-            using (db = new FancyStoreEntities())
+            List<CartItem> cart;
+
+            item.ProductName = db.Products.Find(item.ProductID).ProductName;
+            item.ColorName = db.Colors.Find(item.ColorID).ColorName;
+            item.SizeName = db.Sizes.Find(item.SizeID).SizeName;
+
+            //會員
+            if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
             {
-                List<CartItem> cart;
-
-                item.ProductName = db.Products.Find(item.ProductID).ProductName;
-                item.ColorName = db.Colors.Find(item.ColorID).ColorName;
-                item.SizeName = db.Sizes.Find(item.SizeID).SizeName;
-
-                //會員
-                if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
+                int uid = int.Parse(Request.Cookies["upid"].Value);
+                int pcid = db.ProductColors.Where(c => c.ProductID == item.ProductID && c.ColorID == item.ColorID).First().ProductColorID;
+                int psid = db.ProductSizes.Where(c => c.ProductID == item.ProductID && c.SizeID == item.SizeID).First().ProductSizeID;
+                var sameitem = db.Carts.Where(c => c.UserID == uid && c.ProductID == item.ProductID && c.ProductColorID == pcid && c.ProductSizeID == psid).ToList();
+                //購物車內已有相同款式商品
+                if (sameitem.Count() > 0)
                 {
-                    int uid = int.Parse(Request.Cookies["upid"].Value);
-                    int pcid = db.ProductColors.Where(c => c.ProductID == item.ProductID && c.ColorID == item.ColorID).First().ProductColorID;
-                    int psid = db.ProductSizes.Where(c => c.ProductID == item.ProductID && c.SizeID == item.SizeID).First().ProductSizeID;
-                    var sameitem = db.Carts.Where(c => c.UserID == uid && c.ProductID == item.ProductID && c.ProductColorID == pcid && c.ProductSizeID == psid).ToList();
-                    //購物車內已有相同款式商品
-                    if (sameitem.Count() > 0)
+                    sameitem.First().Quantity += item.OrderQTY;
+                    db.Entry(sameitem.First()).State = System.Data.Entity.EntityState.Modified;
+                }
+                //購物車內無相同款式
+                else
+                {
+                    Cart cartitem = new Cart()
                     {
-                        sameitem.First().Quantity += item.OrderQTY;
-                        db.Entry(sameitem.First()).State = System.Data.Entity.EntityState.Modified;
+                        UserID = uid,
+                        ProductID = item.ProductID,
+                        ProductColorID = pcid,
+                        ProductSizeID = psid,
+                        Quantity = item.OrderQTY,
+                        UnitPrice = item.UnitPrice
+                    };
+                    db.Carts.Add(cartitem);
+                }
+                db.SaveChanges();
+                //把db.cart內容回傳cart>
+                cart = ProductMethod.Cart(uid);
+                return Json(cart, JsonRequestBehavior.AllowGet);
+            }
+            //非會員
+            else
+            {
+                //已有購物車(非購買第一件商品)
+                if (Session["cart"] != null)
+                {
+                    cart = (List<CartItem>)Session["cart"];
+                    var incart = cart.Where(c => c.ProductID == item.ProductID && c.ColorID == item.ColorID && c.SizeID == item.SizeID).ToList();
+                    //購物車內已有相同款式商品
+                    if (incart.Count() > 0)
+                    {
+                        incart.First().OrderQTY += item.OrderQTY;
                     }
                     //購物車內無相同款式
                     else
                     {
-                        Cart cartitem = new Cart()
-                        {
-                            UserID = uid,
-                            ProductID = item.ProductID,
-                            ProductColorID = pcid,
-                            ProductSizeID = psid,
-                            Quantity = item.OrderQTY,
-                            UnitPrice = item.UnitPrice
-                        };
-                        db.Carts.Add(cartitem);
+                        cart.Add(item);
                     }
-                    db.SaveChanges();
-                    //把db.cart內容回傳cart>
-                    cart = ProductMethod.Cart(uid);
-                    return Json(cart, JsonRequestBehavior.AllowGet);
+                    Session.Add("cart", cart);
                 }
-                //非會員
+                //尚未產生購物車(購買第一項商品)
                 else
                 {
-                    //已有購物車(非購買第一件商品)
-                    if (Session["cart"] != null)
-                    {
-                        cart = (List<CartItem>)Session["cart"];
-                        var incart = cart.Where(c => c.ProductID == item.ProductID && c.ColorID == item.ColorID && c.SizeID == item.SizeID).ToList();
-                        //購物車內已有相同款式商品
-                        if (incart.Count() > 0)
-                        {
-                            incart.First().OrderQTY += item.OrderQTY;
-                        }
-                        //購物車內無相同款式
-                        else
-                        {
-                            cart.Add(item);
-                        }
-                        Session.Add("cart", cart);
-                    }
-                    //尚未產生購物車(購買第一項商品)
-                    else
-                    {
-                        //建立購物車
-                        cart = new List<CartItem>();
-                        //加入商品
-                        cart.Add(item);
-                        Session.Add("cart", cart);
-                    }
-                    return Json(cart, JsonRequestBehavior.AllowGet);
+                    //建立購物車
+                    cart = new List<CartItem>();
+                    //加入商品
+                    cart.Add(item);
+                    Session.Add("cart", cart);
                 }
+                return Json(cart, JsonRequestBehavior.AllowGet);
             }
+
         }
 
         public ActionResult LoadCart()
         {
-            using (db = new FancyStoreEntities())
+            //會員
+            if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
             {
-                //會員
-                if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
+                List<CartItem> cart;
+                int uid = int.Parse(Request.Cookies["upid"].Value);
+                var usercart = db.Carts.Where(c => c.UserID == uid).ToList();
+                if (usercart.Count() > 0)
                 {
-                    List<CartItem> cart;
-                    int uid = int.Parse(Request.Cookies["upid"].Value);
-                    var usercart = db.Carts.Where(c => c.UserID == uid).ToList();
-                    if (usercart.Count() > 0)
-                    {
-                        //把db.cart內容回傳cart>
-                        cart = ProductMethod.Cart(uid);
-                        return Json(cart, JsonRequestBehavior.AllowGet);
-                    }
-                    else
-                    {
-                        return Json(0, JsonRequestBehavior.AllowGet);
-                    }
+                    //把db.cart內容回傳cart>
+                    cart = ProductMethod.Cart(uid);
+                    return Json(cart, JsonRequestBehavior.AllowGet);
                 }
-                //非會員
                 else
                 {
-                    if (Session["cart"] != null)
-                    {
-                        return Json((List<CartItem>)Session["cart"], JsonRequestBehavior.AllowGet);
-                    }
-                    else
-                    {
-                        return Json(0, JsonRequestBehavior.AllowGet);
-                    }
+                    return Json(0, JsonRequestBehavior.AllowGet);
                 }
             }
+            //非會員
+            else
+            {
+                if (Session["cart"] != null)
+                {
+                    return Json((List<CartItem>)Session["cart"], JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+
         }
 
         public ActionResult AddFavorite(MyFavorite myFavorite)
         {
-            using (db = new FancyStoreEntities())
+            if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
             {
-                if (Session["IsLogin"] != null || Request.Cookies["IsLogin"] != null)
+                int uid = int.Parse(Request.Cookies["upid"].Value);
+                var fav = db.MyFavorites.Where(f => f.UserID == uid && f.ProductID == myFavorite.ProductID).ToList();
+                string addstate;
+                if (fav.Count() > 0)
                 {
-                    int uid = int.Parse(Request.Cookies["upid"].Value);
-                    var fav = db.MyFavorites.Where(f => f.UserID == uid && f.ProductID == myFavorite.ProductID).ToList();
-                    string addstate;
-                    if (fav.Count() > 0)
-                    {
-                        db.MyFavorites.Remove(fav.First());
-                        addstate = "remove";
-                    }
-                    else
-                    {
-                        myFavorite.UserID = uid;
-                        db.MyFavorites.Add(myFavorite);
-                        addstate = "add";
-                    }
-                    db.SaveChanges();
-                    return Json(addstate, JsonRequestBehavior.AllowGet);
+                    db.MyFavorites.Remove(fav.First());
+                    addstate = "remove";
                 }
                 else
                 {
-                    return RedirectToAction("Index", "Login", new { area = "Members" });
+                    myFavorite.UserID = uid;
+                    db.MyFavorites.Add(myFavorite);
+                    addstate = "add";
                 }
+                db.SaveChanges();
+                return Json(addstate, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login", new { area = "Members" });
             }
         }
-
-        //public ActionResult LoadEvaluation(int productid)
-        //{
-        //    var ordernums = db.ProductEvaluations.Where(e => e.ProductID == productid).Select(e=>e.order);
-        //}
     }
 }
--- FancyWeb/FancyWeb/Areas/ProductDisplay/Functions/ProductMethod.cs
+++ FancyWeb/FancyWeb/Areas/ProductDisplay/Functions/ProductMethod.cs
@@ -1,4 +1,5 @@
-﻿using FancyWeb.Models;
+﻿using FancyWeb.Areas.ProductDisplay.ViewModels;
+using FancyWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,46 +11,46 @@ namespace FancyWeb.Areas.ProductDisplay.Models
     {
         public static List<CartItem> Cart(int id)
         {
-            using (FancyStoreEntities db = new FancyStoreEntities())
-            {
-                //回傳要顯示的購物車資料
-                List<CartItem> cart;
-                var usercart = db.Carts.Where(c => c.UserID == id).ToList();
-                cart = new List<CartItem>();
-                CartItem cartItem;
+            FancyStoreEntities db = new FancyStoreEntities();
+            //回傳要顯示的購物車資料
+            List<CartItem> cart;
+            var usercart = db.Carts.Where(c => c.UserID == id).ToList();
+            cart = new List<CartItem>();
+            CartItem cartItem;
 
-                foreach (var uitem in usercart)
+            foreach (var uitem in usercart)
+            {
+                cartItem = new CartItem()
                 {
-                    cartItem = new CartItem()
-                    {
-                        ProductID = uitem.ProductID,
-                        ProductName = db.Products.Find(uitem.ProductID).ProductName,
-                        ProductColorID = uitem.ProductColorID,
-                        ColorID = db.ProductColors.Find(uitem.ProductColorID).Color.ColorID,
-                        ColorName = db.ProductColors.Find(uitem.ProductColorID).Color.ColorName,
-                        ProductSizeID = uitem.ProductSizeID,
-                        SizeID = db.ProductSizes.Find(uitem.ProductSizeID).Size.SizeID,
-                        SizeName = db.ProductSizes.Find(uitem.ProductSizeID).Size.SizeName,
-                        OrderQTY = uitem.Quantity,
-                        UnitPrice = uitem.UnitPrice,
-                    };
-                    var inactivity = db.ActivityProducts.Where(a => a.ProductID == uitem.ProductID).ToList();
-                    if (inactivity.Count() > 0)
-                    {
-                        cartItem.SUnitPrice = Convert.ToInt32(Math.Floor(inactivity.First().Activity.DiscountMethod.Discount * uitem.UnitPrice));
-                        cartItem.DiscountID = inactivity.First().Activity.DiscountID;
-                        cartItem.ActivityName = inactivity.First().Activity.ActivityName;
-                    }
-                    cart.Add(cartItem);
+                    ProductID = uitem.ProductID,
+                    ProductName = db.Products.Find(uitem.ProductID).ProductName,
+                    ProductColorID = uitem.ProductColorID,
+                    ColorID = db.ProductColors.Find(uitem.ProductColorID).Color.ColorID,
+                    ColorName = db.ProductColors.Find(uitem.ProductColorID).Color.ColorName,
+                    ProductSizeID = uitem.ProductSizeID,
+                    SizeID = db.ProductSizes.Find(uitem.ProductSizeID).Size.SizeID,
+                    SizeName = db.ProductSizes.Find(uitem.ProductSizeID).Size.SizeName,
+                    OrderQTY = uitem.Quantity,
+                    UnitPrice = uitem.UnitPrice,
+                };
+                var inactivity = db.ActivityProducts.Where(a => a.ProductID == uitem.ProductID).ToList();
+                if (inactivity.Count() > 0)
+                {
+                    cartItem.SUnitPrice = Convert.ToInt32(Math.Floor(inactivity.First().Activity.DiscountMethod.Discount * uitem.UnitPrice));
+                    cartItem.DiscountID = inactivity.First().Activity.DiscountID;
+                    cartItem.ActivityName = inactivity.First().Activity.ActivityName;
                 }
-                return cart;
+                cart.Add(cartItem);
             }
+            return cart;
         }
 
         public static IQueryable<ProductCell> CreateProductCells(IQueryable<ProductColor> preproducts)
         {
-            var products = preproducts.Select(p => new ProductCell
-            //var products = preproducts.Where(p => p.Product.ProductInDate >= DateTime.Now && p.Product.ProductOutDate <= DateTime.Now).Select(p => new ProductCell
+            FancyStoreEntities db = new FancyStoreEntities();
+
+            //var products = preproducts.Select(p => new ProductCell
+            var products = preproducts.Where(p => p.Product.ProductInDate <= DateTime.Now && p.Product.ProductOutDate >= DateTime.Now).Select(p => new ProductCell
             {
                 ProductID = p.Product.ProductID,
                 ProductName = p.Product.ProductName,
@@ -86,6 +87,35 @@ namespace FancyWeb.Areas.ProductDisplay.Models
                     break;
             }
             return products;
+        }
+
+        public static List<EvaluationDisplay> LoadEvaluation(int id)
+        {
+            FancyStoreEntities db = new FancyStoreEntities();
+
+            var ordernums = db.ProductEvaluations.Where(e => e.ProductID == id);
+            List<EvaluationDisplay> evaluations = new List<EvaluationDisplay>();
+            EvaluationDisplay evaluation;
+            foreach (var ordernum in ordernums)
+            {
+                string grade = null;
+                for (int i = 1; i <= ordernum.Grade; i++)
+                {
+                    grade+="✩";
+                }
+                evaluation = new EvaluationDisplay
+                {
+                    UserPhoto = Convert.ToBase64String(db.Users.Find(ordernum.UserID).Photo.Photo1),
+                    Comment = ordernum.Comment,
+                    Grade = grade,
+                    OrderNum = ordernum.OrderNum,
+                    EvaluationDate = ordernum.EvaluationDate.ToString("yyyy/MM/dd"),
+                    ColorName = db.ProductColors.Where(c => c.ProductID == ordernum.ProductID).FirstOrDefault().Color.ColorName,
+                    SizeName = db.ProductSizes.Where(s => s.ProductID == ordernum.ProductID).FirstOrDefault().Size.SizeName
+                };
+                evaluations.Add(evaluation);
+            }
+            return evaluations;
         }
     }
 }
