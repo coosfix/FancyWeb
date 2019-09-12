4915df31c6f49269ae785cb6b806a5e9c506b5ab
ewingtsui <ewing.tsui@gmail.com>
2019-09-12 16:31:17 +0800

    Ewing 20190912 - 增加Activity上傳圖片

--- FancyWeb/FancyWeb/Areas/Backend/ViewModels/ShipmaneAllViewModel.cs
+++ FancyWeb/FancyWeb/Areas/Backend/ViewModels/ShipmaneAllViewModel.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace FancyWeb.Areas.Backend.ViewModels
 {
     public class ShipmaneAllViewModel
-    {
+    {        
         public int ShippingID { get; set; }
         public int OrderID { get; set; }
+
         public ShipHeaderViewModel shipheader;
         public IEnumerable<ShipDetailViewModel> shipdetailList;
     }
